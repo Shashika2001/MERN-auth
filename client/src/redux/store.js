ebb0b391e8175e6dabfc6ab681a0559e398b6600
Shashika2001 <ekanayakespm@gmail.com>
2024-12-06 23:55:55 +0530

    didnt complete the issue

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,5 +1,12 @@
-import {configureStore } from '@reduxjs/toolkit';
+import {combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice.js';
+
+
+const rootReducer = combineReducers({
+    user: userReducer,
+});
+
+const persisted = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
     reducer: {user: userReducer},
