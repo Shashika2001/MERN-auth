cdae0eabdfaf965b28fafd23c3435248525f954a
Shashika2001 <ekanayakespm@gmail.com>
2024-12-27 23:33:55 +0530

    complete user update functionality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -24,9 +24,27 @@ const userSlice = createSlice({
             state.loading = false;
             state.error = action.payload;
         },
+        updateUserStart: (state) => {
+            state.loading = true;
+        },
+        updateUserSuccess: (state, action) => {
+            state.currentUser = action.payload;
+            state.loading = false;
+            state.error = false;
+        },
+        updateUserFailure: (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+        },
     }
 });
 
-export const {signINStart, signINSuccess, signINFailure} = userSlice.actions;
+export const {signINStart,
+     signINSuccess,
+      signINFailure,
+      updateUserStart,
+      updateUserSuccess,
+      updateUserFailure
+    } = userSlice.actions;
 
 export default userSlice.reducer;
